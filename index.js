6412d75f9a6ace80659f0994da7d7ff5cdaca117
Andreas <aw@decaf.de>
2015-08-04 14:04:18 +0200

    reading empty attributes as true

--- index.js
+++ index.js
@@ -3,57 +3,60 @@ var rbrace = /(?:\{[\s\S]*\}|\[[\s\S]*\])$/;
 
 
 function camelize(string) {
-	return string.toLowerCase().replace(/-(.)/g, function (match, group1) {
-		return group1.toUpperCase();
-	});
+    return string.toLowerCase().replace(/-(.)/g, function (match, group1) {
+        return group1.toUpperCase();
+    });
 }
 
 // Inspired by jQuery. See https://github.com/jquery/jquery/blob/master/src/data.js
 function getValue(value) {
-	var data = value,
-		result = value;
+    var data   = value,
+        result = value;
 
-	if (typeof data === 'string') {
-		try {
-			if (data === 'true') {
-				result = true;
-			} else if (data === 'false') {
-				result = false;
-			} else if (data === 'null') {
-				result = null;
-			} else if (+data + '' === data) {
-				result = +data;
-			} else {
-				result = rbrace.test(data) ? JSON.parse(data) : data;
-			}
-		} catch (error) {
-		}
-	}
-	return result;
+    if (typeof data === 'string') {
+        try {
+            if (!data || data === 'true') {
+                result = true;
+            } else if (data === 'false') {
+                result = false;
+            } else if (data === 'null') {
+                result = null;
+            } else if (+data + '' === data) {
+                result = +data;
+            } else {
+                result = rbrace.test(data) ? JSON.parse(data) : data;
+            }
+        } catch (error) {
+        }
+    }
+    return result;
 }
 
-function parseAttrs(el) {
-	var attributes = el.attributes,
-		dataHash = {};
+function parseAttrs(gremlin) {
+    var el         = gremlin.el;
+    var attributes = el.attributes;
+    var dataHash   = {};
+    var propsHash  = {};
 
-	if (attributes !== null && attributes !== undefined) {
-		for (let i = 0; i < attributes.length; i++) {
-			var attr = attributes[i];
+    if (attributes !== null && attributes !== undefined) {
+        for (let i = 0; i < attributes.length; i++) {
+            var attr = attributes[i];
 
-			if (attr.name.substr(0, 5) !== 'data-') {
-				continue;
-			}
+            if (attr.name.substr(0, 5) !== 'data-') {
+                propsHash[camelize(attr.name)] = getValue(attr.value);
+            } else {
+                dataHash[camelize(attr.name.substr(5))] = getValue(attr.value);
+            }
+        }
 
-			dataHash[camelize(attr.name.substr(5))] = getValue(attr.value);
-		}
-
-	}
-	return dataHash;
+    }
+    gremlin.data = dataHash;
+    gremlin.props = propsHash;
 }
 
 
 module.exports = {
-	initialize() {
-		this.data = parseAttrs(this.el);
-	}
+    initialize() {
+        parseAttrs(this);
+    }
 };
