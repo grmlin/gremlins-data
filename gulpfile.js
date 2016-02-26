4e20507ad8301388ca4bbb3b21b4749e27095504
Andreas <iam@grml.in>
2016-02-26 17:31:09 +0100

    new babel task

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,45 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
-var version = require('./package.json').version;
-var rename = require("gulp-rename");
 var through2 = require('through2');
 var browserify = require('browserify');
+var babel = require('gulp-babel');
 
-
-gulp.task('scriptsTest', function () {
-	return gulp.src('test/src/gremlins-data.js')
-		.pipe(through2.obj(function (file, enc, next) {
-			browserify(file.path, {
-				standalone: 'gremlinsData',
-				debug: false
-			})
-				.transform('babelify')
-				.bundle(function (err, res) {
-					// assumes file.contents is a Buffer
-					file.contents = res;
-					next(null, file);
-				});
-		}))
-		.pipe(gulp.dest('./test/specs'));
+gulp.task("babel", function () {
+    return gulp.src("src/**/*.js")
+        .pipe(babel())
+        .pipe(gulp.dest("lib"));
 });
 
-gulp.task('scripts', function () {
-	return gulp.src('./index.js')
-		.pipe(through2.obj(function (file, enc, next) {
-			browserify(file.path, {
-				standalone: 'gremlinsData',
-				debug: false
-			})
-				.transform('babelify')
-				.bundle(function (err, res) {
-					// assumes file.contents is a Buffer
-					file.contents = res;
-					next(null, file);
-				});
-		}))
-		.pipe(rename('gremlins-data.js'))
-		.pipe(gulp.dest('./dist'));
+gulp.task('scriptsTest', ['babel'], function () {
+    return gulp.src('test/src/gremlins-data.js')
+        .pipe(through2.obj(function (file, enc, next) {
+            browserify(file.path, {
+                debug: false
+            })
+                .bundle(function (err, res) {
+                    // assumes file.contents is a Buffer
+                    file.contents = res;
+                    next(null, file);
+                });
+        }))
+        .pipe(gulp.dest('./test/specs'));
 });
 
 gulp.task('connect', function () {
-	connect.server({
-		root: ['test'],
-		port: 8000,
-		livereload: false
-	});
+    connect.server({
+        root: ['test'],
+        port: 8000,
+        livereload: false
+    });
 });
 
 gulp.task("reload", function () {
-	gulp.src('lib/watched.js')
-		.pipe(connect.reload());
+    gulp.src('lib/index.js')
+        .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
-	gulp.watch(['./index.js', './test/src/*.*'], ['scriptsTest', 'reload']);
+    gulp.watch(['./src/index.js', './test/src/*.*'], ['scriptsTest', 'reload']);
 });
 
 gulp.task('default', ['connect', 'scriptsTest', 'watch']);
